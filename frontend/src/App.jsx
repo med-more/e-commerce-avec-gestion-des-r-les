2cb17793a75c5967bd1d9ee930986fa5ad32213f
Mohammed Baba <mbfmz20@gmail.com>
2025-03-04 11:54:42 +0000

    setup environement frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+   <>
+    <h1 className="text-red-600">
+      Hello world!
+    </h1>
+   </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Admin/displayProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const displayProduct = () => {
+  return (
+    <div>displayProduct</div>
+  )
+}
+
+export default displayProduct
